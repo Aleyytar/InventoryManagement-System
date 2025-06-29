a6b5aa64162ff2bd6f58cea718209dad4590df80
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2025-06-29 22:54:11 +0300

    FrontOFManageUserCreated

---
+++ InventoryManagement System/ManageUsers.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace InventoryManagement_System
+{
+    partial class ManageUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Crimson;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1301, 125);
+            panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.Location = new Point(1252, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 54);
+            label3.TabIndex = 2;
+            label3.Text = "X";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label2.Location = new Point(286, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(710, 60);
+            label2.TabIndex = 1;
+            label2.Text = "INVENTORY MANAGMENT SYSTEM";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(505, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(283, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Manage User ";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            textBox1.ForeColor = Color.Crimson;
+            textBox1.Location = new Point(26, 157);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(322, 34);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "UserName";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            textBox2.ForeColor = Color.Crimson;
+            textBox2.Location = new Point(26, 258);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(322, 34);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "Password";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            textBox3.ForeColor = Color.Crimson;
+            textBox3.Location = new Point(26, 310);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(322, 34);
+            textBox3.TabIndex = 3;
+            textBox3.Text = "Telephone";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            textBox4.ForeColor = Color.Crimson;
+            textBox4.Location = new Point(26, 208);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(322, 34);
+            textBox4.TabIndex = 4;
+            textBox4.Text = "FullName";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(26, 365);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 46);
+            button1.TabIndex = 5;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Crimson;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(135, 365);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 46);
+            button2.TabIndex = 6;
+            button2.Text = "Edit";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Crimson;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(254, 365);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 46);
+            button3.TabIndex = 7;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Crimson;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(135, 436);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 46);
+            button4.TabIndex = 8;
+            button4.Text = "Home";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(381, 157);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(908, 325);
+            dataGridView1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Crimson;
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 520);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1301, 37);
+            panel2.TabIndex = 10;
+            // 
+            // ManageUsers
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1301, 557);
+            Controls.Add(panel2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ManageUsers";
+            Text = "ManageUsers";
+            Load += ManageUsers_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private Panel panel2;
+    }
+}
---
+++ InventoryManagement System/ManageUsers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement_System
+{
+    public partial class ManageUsers : Form
+    {
+        public ManageUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageUsers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
