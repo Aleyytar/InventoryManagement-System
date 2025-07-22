f6d5c30bcf250a39af56d73516292cba9540c311
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2025-07-22 22:14:38 +0300

    OrdersDetails

--- InventoryManagement System/ManageOrders.Designer.cs
+++ InventoryManagement System/ManageOrders.Designer.cs
@@ -36,8 +36,25 @@
             panel2 = new Panel();
             label4 = new Label();
             CustomerListGV = new DataGridView();
+            OrderIdTb = new TextBox();
+            CustomerIdTb = new TextBox();
+            orderDatetime = new DateTimePicker();
+            label5 = new Label();
+            Catbox = new ComboBox();
+            ProductListGV = new DataGridView();
+            QuantityTb = new TextBox();
+            label6 = new Label();
+            button4 = new Button();
+            OrderGV = new DataGridView();
+            CustomerNameTb = new TextBox();
+            label7 = new Label();
+            TotalPriceLb = new Label();
+            button1 = new Button();
+            button2 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)CustomerListGV).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ProductListGV).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)OrderGV).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -60,7 +77,7 @@
             button6.FlatAppearance.BorderSize = 0;
             button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
             button6.ForeColor = Color.White;
-            button6.Location = new Point(12, 72);
+            button6.Location = new Point(3, 72);
             button6.Name = "button6";
             button6.Size = new Size(296, 46);
             button6.TabIndex = 7;
@@ -103,7 +120,7 @@
             // 
             panel2.BackColor = Color.Crimson;
             panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 673);
+            panel2.Location = new Point(0, 766);
             panel2.Name = "panel2";
             panel2.Size = new Size(1383, 23);
             panel2.TabIndex = 5;
@@ -113,7 +130,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Sitka Heading", 18F, FontStyle.Regular, GraphicsUnit.Point, 162);
             label4.ForeColor = Color.RoyalBlue;
-            label4.Location = new Point(158, 143);
+            label4.Location = new Point(156, 128);
             label4.Name = "label4";
             label4.Size = new Size(233, 43);
             label4.TabIndex = 26;
@@ -123,18 +140,200 @@
             // 
             CustomerListGV.BackgroundColor = Color.White;
             CustomerListGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            CustomerListGV.Location = new Point(21, 189);
+            CustomerListGV.Location = new Point(26, 174);
             CustomerListGV.Name = "CustomerListGV";
             CustomerListGV.RowHeadersWidth = 51;
-            CustomerListGV.Size = new Size(532, 271);
+            CustomerListGV.Size = new Size(532, 227);
             CustomerListGV.TabIndex = 27;
             CustomerListGV.CellContentClick += CustomerListGV_CellContentClick;
+            // 
+            // OrderIdTb
+            // 
+            OrderIdTb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            OrderIdTb.ForeColor = Color.Crimson;
+            OrderIdTb.Location = new Point(26, 424);
+            OrderIdTb.Name = "OrderIdTb";
+            OrderIdTb.Size = new Size(322, 34);
+            OrderIdTb.TabIndex = 28;
+            OrderIdTb.Text = "OrderId";
+            // 
+            // CustomerIdTb
+            // 
+            CustomerIdTb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            CustomerIdTb.ForeColor = Color.Crimson;
+            CustomerIdTb.Location = new Point(26, 477);
+            CustomerIdTb.Name = "CustomerIdTb";
+            CustomerIdTb.Size = new Size(322, 34);
+            CustomerIdTb.TabIndex = 29;
+            CustomerIdTb.Text = "CustomerId";
+            // 
+            // orderDatetime
+            // 
+            orderDatetime.CalendarFont = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            orderDatetime.CalendarForeColor = Color.Crimson;
+            orderDatetime.CalendarMonthBackground = Color.Crimson;
+            orderDatetime.Location = new Point(26, 604);
+            orderDatetime.Name = "orderDatetime";
+            orderDatetime.Size = new Size(250, 27);
+            orderDatetime.TabIndex = 30;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Heading", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label5.ForeColor = Color.RoyalBlue;
+            label5.Location = new Point(26, 577);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 24);
+            label5.TabIndex = 31;
+            label5.Text = "Order Date";
+            // 
+            // Catbox
+            // 
+            Catbox.Font = new Font("Segoe UI", 12F);
+            Catbox.ForeColor = Color.Crimson;
+            Catbox.FormattingEnabled = true;
+            Catbox.Location = new Point(879, 135);
+            Catbox.Name = "Catbox";
+            Catbox.Size = new Size(226, 36);
+            Catbox.TabIndex = 33;
+            Catbox.Text = " Product Category";
+            Catbox.SelectedIndexChanged += Catbox_SelectedIndexChanged;
+            // 
+            // ProductListGV
+            // 
+            ProductListGV.BackgroundColor = Color.White;
+            ProductListGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ProductListGV.Location = new Point(564, 174);
+            ProductListGV.Name = "ProductListGV";
+            ProductListGV.RowHeadersWidth = 51;
+            ProductListGV.Size = new Size(807, 227);
+            ProductListGV.TabIndex = 32;
+            ProductListGV.CellContentClick += ProductGV_CellContentClick;
+            // 
+            // QuantityTb
+            // 
+            QuantityTb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            QuantityTb.ForeColor = Color.Crimson;
+            QuantityTb.Location = new Point(759, 417);
+            QuantityTb.Name = "QuantityTb";
+            QuantityTb.Size = new Size(322, 34);
+            QuantityTb.TabIndex = 34;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sitka Heading", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label6.ForeColor = Color.RoyalBlue;
+            label6.Location = new Point(681, 424);
+            label6.Name = "label6";
+            label6.Size = new Size(72, 24);
+            label6.TabIndex = 35;
+            label6.Text = "Quantity";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Crimson;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(1087, 410);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 49);
+            button4.TabIndex = 36;
+            button4.Text = " Add To";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // OrderGV
+            // 
+            OrderGV.BackgroundColor = Color.White;
+            OrderGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            OrderGV.Location = new Point(376, 477);
+            OrderGV.Name = "OrderGV";
+            OrderGV.RowHeadersWidth = 51;
+            OrderGV.Size = new Size(1004, 229);
+            OrderGV.TabIndex = 37;
+            OrderGV.CellContentClick += OrderGV_CellContentClick;
+            // 
+            // CustomerNameTb
+            // 
+            CustomerNameTb.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            CustomerNameTb.ForeColor = Color.Crimson;
+            CustomerNameTb.Location = new Point(26, 530);
+            CustomerNameTb.Name = "CustomerNameTb";
+            CustomerNameTb.Size = new Size(322, 34);
+            CustomerNameTb.TabIndex = 38;
+            CustomerNameTb.Text = "CustomerName";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Sitka Heading", 10.1999989F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label7.ForeColor = Color.RoyalBlue;
+            label7.Location = new Point(803, 723);
+            label7.Name = "label7";
+            label7.Size = new Size(81, 24);
+            label7.TabIndex = 39;
+            label7.Text = "TotalPrice";
+            // 
+            // TotalPriceLb
+            // 
+            TotalPriceLb.AutoSize = true;
+            TotalPriceLb.Font = new Font("SimSun-ExtB", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            TotalPriceLb.ForeColor = Color.Black;
+            TotalPriceLb.Location = new Point(890, 723);
+            TotalPriceLb.Name = "TotalPriceLb";
+            TotalPriceLb.Size = new Size(23, 23);
+            TotalPriceLb.TabIndex = 40;
+            TotalPriceLb.Text = "$";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 657);
+            button1.Name = "button1";
+            button1.Size = new Size(164, 49);
+            button1.TabIndex = 41;
+            button1.Text = "Insert Order";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Crimson;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(183, 657);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 49);
+            button2.TabIndex = 42;
+            button2.Text = "View Orders";
+            button2.UseVisualStyleBackColor = false;
             // 
             // ManageOrders
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1383, 696);
+            ClientSize = new Size(1383, 789);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(TotalPriceLb);
+            Controls.Add(label7);
+            Controls.Add(CustomerNameTb);
+            Controls.Add(OrderGV);
+            Controls.Add(button4);
+            Controls.Add(label6);
+            Controls.Add(QuantityTb);
+            Controls.Add(Catbox);
+            Controls.Add(ProductListGV);
+            Controls.Add(label5);
+            Controls.Add(orderDatetime);
+            Controls.Add(CustomerIdTb);
+            Controls.Add(OrderIdTb);
             Controls.Add(CustomerListGV);
             Controls.Add(label4);
             Controls.Add(panel2);
@@ -146,6 +345,8 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)CustomerListGV).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ProductListGV).EndInit();
+            ((System.ComponentModel.ISupportInitialize)OrderGV).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -160,5 +361,20 @@
         private Panel panel2;
         private Label label4;
         private DataGridView CustomerListGV;
+        private TextBox OrderIdTb;
+        private TextBox CustomerIdTb;
+        private DateTimePicker orderDatetime;
+        private Label label5;
+        private ComboBox Catbox;
+        private DataGridView ProductListGV;
+        private TextBox QuantityTb;
+        private Label label6;
+        private Button button4;
+        private DataGridView OrderGV;
+        private TextBox CustomerNameTb;
+        private Label label7;
+        private Label TotalPriceLb;
+        private Button button1;
+        private Button button2;
     }
 }
--- InventoryManagement System/ManageOrders.cs
+++ InventoryManagement System/ManageOrders.cs
@@ -18,6 +18,51 @@ namespace InventoryManagement_System
             InitializeComponent();
         }
         SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-B6S9L8P\SQLEXPRESS;Initial Catalog=inventorymgdb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+        DataTable table = new DataTable();
+        int num = 0;
+        int uprice, qty, totprice;
+        string product;
+
+        void fillCategory()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM CategoriesTbl", con);
+                SqlDataReader rdr;
+                rdr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Columns.Add("CatName", typeof(string));
+                dt.Load(rdr);
+                Catbox.ValueMember = "CatName";
+                Catbox.DataSource = dt;
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+        private void populateProduct()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM ProductssTbl";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+                var ds = new DataSet();
+                sda.Fill(ds);
+                ProductListGV.DataSource = ds.Tables[0];
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
         private void populate()
         {
             try
@@ -37,21 +82,21 @@ namespace InventoryManagement_System
             }
         }
 
-        private void ProductdataGridStyle()
+        private void StyleDataGridView(DataGridView dgv)
         {
-            CustomerListGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            CustomerListGV.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            CustomerListGV.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            CustomerListGV.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            CustomerListGV.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            CustomerListGV.RowHeadersVisible = false;
-            CustomerListGV.EnableHeadersVisualStyles = false;
-            CustomerListGV.ColumnHeadersDefaultCellStyle.BackColor = Color.Crimson;
-            CustomerListGV.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            CustomerListGV.DefaultCellStyle.SelectionBackColor = Color.LightPink;
-            CustomerListGV.DefaultCellStyle.SelectionForeColor = Color.Black;
-            CustomerListGV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            CustomerListGV.MultiSelect = false;
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgv.RowHeadersVisible = false;
+            dgv.EnableHeadersVisualStyles = false;
+            dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.Crimson;
+            dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgv.DefaultCellStyle.SelectionBackColor = Color.LightPink;
+            dgv.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false;
         }
         private void label3_Click(object sender, EventArgs e)
         {
@@ -61,7 +106,16 @@ namespace InventoryManagement_System
         private void ManageOrders_Load(object sender, EventArgs e)
         {
             populate();
-            ProductdataGridStyle();
+            StyleDataGridView(CustomerListGV);
+            StyleDataGridView(ProductListGV);
+            StyleDataGridView(OrderGV);
+            populateProduct();
+            fillCategory();
+            table.Columns.Add("num", typeof(int));
+            table.Columns.Add("product", typeof(string));
+            table.Columns.Add("qty", typeof(int));
+            table.Columns.Add("uprice", typeof(int));
+            table.Columns.Add("totprice", typeof(int));
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -81,7 +135,130 @@ namespace InventoryManagement_System
 
         private void CustomerListGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            CustomerIdTb.Text = CustomerListGV.SelectedRows[0].Cells[0].Value.ToString();
+            CustomerNameTb.Text = CustomerListGV.SelectedRows[0].Cells[1].Value.ToString();
+        }
+        int flag = 0;
+        private void ProductGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            product = ProductListGV.SelectedRows[0].Cells[1].Value.ToString();
+            // qty = Convert.ToInt32(QuantityTb.Text);
+            uprice = Convert.ToInt32(ProductListGV.SelectedRows[0].Cells[3].Value.ToString());
+            // totprice = qty * uprice;
+            flag = 1;
 
+        }
+
+        private void Catbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedCategory = Catbox.Text;
+
+                if (string.IsNullOrWhiteSpace(selectedCategory))
+                    return;
+
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT P.ProductId, P.ProductName, P.ProductQty, P.ProductPrice, P.ProductDesc, C.CatName " +
+                    "FROM ProductssTbl P " +
+                    "JOIN CategoriesTbl C ON P.CatId = C.CatId " +
+                    "WHERE C.CatName = @CatName", con);
+
+                cmd.Parameters.AddWithValue("@CatName", selectedCategory);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                ProductListGV.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(QuantityTb.Text) || !int.TryParse(QuantityTb.Text, out qty))
+            {
+                MessageBox.Show("Enter The Quantity  Of Product");
+
+            }
+            else if (flag == 0)
+            {
+                MessageBox.Show("Select the Product");
+
+
+
+
+            }
+            else if (Convert.ToInt32(QuantityTb.Text) <= 0)
+            {
+                MessageBox.Show("Quantity must be greater than 0");
+            }
+            else if (Convert.ToInt32(QuantityTb.Text) > Convert.ToInt32(ProductListGV.SelectedRows[0].Cells[2].Value.ToString()))
+            {
+                MessageBox.Show("Not Enough Product In Stock");
+            }
+            else if (OrderIdTb.Text == "")
+            {
+                MessageBox.Show("Please Enter Order Id");
+            }
+            else if (CustomerIdTb.Text == "")
+            {
+                MessageBox.Show("Please Select Customer");
+            }
+
+            else
+            {
+                num = num + 1;
+                qty = Convert.ToInt32(QuantityTb.Text);
+                totprice = qty * uprice;
+                table.Rows.Add(num, product, qty, uprice, totprice);
+                totprice = qty * uprice;
+                OrderGV.DataSource = table;
+                flag = 0;
+            }
+            int grandTotal = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                grandTotal += Convert.ToInt32(row["totprice"]);
+            }
+            TotalPriceLb.Text = "Total: $" + grandTotal.ToString();
+            try
+            {
+                con.Open();
+                SqlCommand updateCmd = new SqlCommand("UPDATE ProductssTbl SET ProductQty = ProductQty - @SoldQty WHERE ProductName = @ProductName", con);
+                updateCmd.Parameters.AddWithValue("@SoldQty", qty);
+                updateCmd.Parameters.AddWithValue("@ProductName", product);
+                updateCmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Stok güncelleme hatası: " + ex.Message);
+            }
+            populateProduct();
+        }
+
+        private void OrderGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            OrderIdTb.Text = OrderGV.SelectedRows[0].Cells[0].Value.ToString();
         }
     }
 }
