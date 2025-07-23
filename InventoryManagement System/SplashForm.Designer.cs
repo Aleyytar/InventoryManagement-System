46844e910cd30056cc0ddc33c936c4ceb798745f
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2025-07-23 14:27:06 +0300

    SplashForm added

---
+++ InventoryManagement System/SplashForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace InventoryManagement_System
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label2 = new Label();
+            label3 = new Label();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(45, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(710, 60);
+            label2.TabIndex = 2;
+            label2.Text = "INVENTORY MANAGMENT SYSTEM";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(580, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 38);
+            label3.TabIndex = 4;
+            label3.Text = "Version1.0";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(57, 209);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(708, 25);
+            progressBar1.TabIndex = 5;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // SplashForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Crimson;
+            ClientSize = new Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SplashForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SplashForm";
+            Load += SplashForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label3;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ InventoryManagement System/SplashForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement_System
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+        int counter = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter += 5;
+            progressBar1.Value = counter;
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                this.Hide();
+                Form1 home = new Form1();
+                home.Show();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
